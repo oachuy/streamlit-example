7a4153955760e799679641a98b45ac35003b3407
oachuy <oachuy@gmail.com>
2023-07-11 12:51:11 -0500

    Update 11 streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_graph(data):
+    # Select the top 10 rows based on TF-IDF score
+    data_top10 = data.nlargest(10, 'Score')
+
     fig, ax = plt.subplots()
-    ax.barh(data['Term'], data['Score'])
-    ax.set_xlabel('TF-IDF Score')
+    ax.barh(data_top10['Term'], data_top10['Score'])
+    ax.set_xlabel('Score')
     ax.set_ylabel('Term')
-    ax.set_title('TF-IDF Scores')
+    ax.set_title('Top 10 Scores')
     st.pyplot(fig)
 
 def main():
     st.title("TF-IDF Visualization")
+
     # Load data
     filename = 'output_tf_idf_file.txt'
-    df = pd.read_csv(filename, sep=":")
+    df = pd.read_csv(filename)
 
     # Display the raw data
     st.subheader("Raw Data")
     st.dataframe(df)
-    st.write("Column Names:", df.columns)
 
     # Plot the graph
     plot_graph(df)
