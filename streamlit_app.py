f097dda508e08287949ba73666aa6772980d4552
oachuy <oachuy@gmail.com>
2023-07-14 14:25:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,10 @@ def main():
         
     # Display the raw data
     #st.subheader("Raw Data")
-    df=st.dataframe(words)
+    st.dataframe(words)
         
     # Plot the graph
-    plot_graph(df)
+    #plot_graph(df)
 
 if __name__ == '__main__':
     main()
